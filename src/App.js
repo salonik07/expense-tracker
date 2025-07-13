af7849869b96f330d1745baa61f8633b49d1e904
salonik07 <144094717+salonik07@users.noreply.github.com>
2025-07-14 03:59:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+
+import { Typography, Box, styled } from '@mui/material';
+import './App.css';
+
+import Balance from './Components/Balance';
+import ExpenseCard from './Components/ExpenseCard';
+import Transactions from './Components/Transactions';
+import NewTransaction from './Components/NewTransaction';
+
+const Header = styled(Typography)`
+  margin: 10px 0;
+  color: blue;
+  font-size: 36px;
+  text-transform: uppercase;
+`;
+
+const Component = styled(Box)`
+  background: #FFF;
+  padding: 10px;
+  border-radius: 20px;
+  display: flex;
+  width: 800px;
+  & > div {
+    padding: 10px;
+    width: 50%;
+    height: 70vh;
+  }
+}
+`;
+
+function App() {
+  
+  const [transactions, setTransactions] = useState([
+    
+  ]);
+
+  const deleteTransaction = (id) => {
+    console.log(id);
+    setTransactions(transactions.filter(transaction => transaction.id !== id));
+    console.log(transactions);
+  }
+
+  const addTransaction = (transaction) => {
+    setTransactions(transactions => [transaction, ...transactions]);
+    console.log(transaction);
+    console.log(transactions);
+  }
+
+
+  return (
+    <div className="App">
+      <Header>Expense Tracker</Header>
+      <Component>
+        <Box>
+          <Balance transactions={transactions} />
+          <ExpenseCard transactions={transactions} />
+          <NewTransaction addTransaction={addTransaction}/>
+        </Box>
+        <Box>
+          <Transactions transactions={transactions} deleteTransaction={deleteTransaction}/>
+        </Box>
+      </Component>
+    </div>
+  );
+}
+
+export default App;
